0b5464dd3c0b1ba42f045b7c576d8f95740dcbf7
Naoki KIMURA <naoki.kimura@flair-agency.biz>
2026-02-21 10:22:54 +0900

    Fix a bug

--- public/assets/common.js
+++ public/assets/common.js
@@ -10,7 +10,7 @@
 
 // Active link highlight on scroll
 (() => {
-  const links = Array.from(document.querySelectorAll('nav.on-page menu li a[href^="#"]'));
+  const links = Array.from(document.querySelectorAll('nav.on-page menu li a[href^="#"]')).map((/** @type HTMLElement */ link) => link);
   const targets = links.map(l => { try { return document.querySelector(l.getAttribute('href')) } catch (e) { return null } }).filter(Boolean);
   const linkMap = new Map(targets.map((el, i) => [el.id, links[i]]));
   const io = new IntersectionObserver((entries) => {
@@ -18,8 +18,30 @@
       const link = linkMap.get(entry.target.id);
       if (!link) return;
       if (entry.isIntersecting) {
-        links.forEach(l => l.classList.remove('active'));
-        link.classList.add('active');
+        links.forEach((link) => {
+          /**
+           * 
+           * @this HTMLElement
+           * @param {string} key
+           * @param {string} value
+           */
+          function removeData(key, value) {
+            this.dataset[key] = (this.dataset[key] ?? '').split(' ').filter((state) => state !== value).join(' ').trim();
+            return value;
+          }
+          removeData.bind(link)('state', 'active');
+        });
+        /**
+         * 
+         * @this HTMLElement
+         * @param {string} key
+         * @param {string} value
+         */
+        function addData(key, value) {
+          this.dataset[key] = (this.dataset[key] ?? '').split(' ').concat(value).join(' ').trim();
+          return value;
+        }
+        addData.bind(link)('state', 'active');
       }
     });
   }, { rootMargin: '-40% 0px -55% 0px', threshold: 0.01 });
